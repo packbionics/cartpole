70c37ab3fb6857ab2aa9b691e995e70b0bfde36d
Jason Xie <jasonxie@live.unc.edu>
2021-10-05 21:56:33 -0400

    add rviz capability

---
+++ cartpole_description/cartpole_description/demo_state_publisher.py
@@ -0,0 +1,71 @@
+from math import pi
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from sensor_msgs.msg import JointState
+from tf2_ros import TransformBroadcaster, TransformStamped
+
+class DemoStatePublisher(Node):
+
+    def __init__(self):
+        rclpy.init()
+        super().__init__('demo_state_publisher')
+
+        qos_profile = QoSProfile(depth=10)
+        self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
+        self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
+        self.nodeName = self.get_name()
+        self.get_logger().info("{0} started".format(self.nodeName))
+
+        degree = pi / 180.0
+        loop_rate = self.create_rate(30)
+
+        # robot state - cart and pole
+        cart = 0.
+        pole = 0.
+
+        cart_inc = 0.01
+        cart_inc_multiplier = 1.0
+        pole_inc = degree
+
+        # message declarations
+        odom_trans = TransformStamped()
+        odom_trans.header.frame_id = 'odom'
+        odom_trans.child_frame_id = 'axis'
+        joint_state = JointState()
+
+        try:
+            while rclpy.ok():
+                rclpy.spin_once(self)
+
+                # update joint_state
+                now = self.get_clock().now()
+                joint_state.header.stamp = now.to_msg()
+                joint_state.name = ['revolute_pole', 'slider_cart']
+                joint_state.position = [pole, cart]
+
+                # send the joint state
+                self.joint_pub.publish(joint_state)
+                self.broadcaster.sendTransform(odom_trans)
+                
+                # Create new robot state
+                if (cart > 0.42):
+                    cart_inc_multiplier = -1.0
+                if (cart < -0.42):
+                    cart_inc_multiplier = 1.0
+
+                cart += cart_inc * cart_inc_multiplier
+
+                pole += pole_inc
+
+                # This will adjust as needed per iteration
+                loop_rate.sleep()
+
+        except KeyboardInterrupt:
+            pass
+
+def main():
+    node = DemoStatePublisher()
+
+if __name__ == '__main__':
+    main()
---
+++ cartpole_description/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'cartpole_description'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('robot/urdf/*')),
+        (os.path.join('share', package_name), glob('rviz/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jasonx',
+    maintainer_email='59701038+JChunX@users.noreply.github.com',
+    description='Cartpole descriptions and simulations',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'demo_state_publisher = cartpole_description.demo_state_publisher:main'
+        ],
+    },
+)
