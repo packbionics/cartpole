6277c727ce7e151ae9e276c767d2991f700e6689
Jason X <59701038+JChunX@users.noreply.github.com>
2021-11-06 02:17:31 -0400

    tweak teleop

--- cartpole_control/cartpole_control/cartpole_teleop_key.py
+++ cartpole_control/cartpole_control/cartpole_teleop_key.py
@@ -1,16 +1,19 @@
 import select
 import sys
 import termios
+import threading
+import time
 import tty
 
 if sys.platform == 'win32':
     import msvcrt
 
 import rclpy
-from std_msgs.msg import Float64
 from cartpole_interfaces.msg import Action
 from cartpole_interfaces.srv import SetPosition, Stop
 from rclpy.node import Node
+from std_msgs.msg import Float64
+from queue import Queue
 
 """
 cartpole_teleop_key
@@ -21,7 +24,7 @@ Additionally, implement a client and sends requests when appropriate
 Subscribes to:
 
 Publishes to:
-    cartpole/action [cartpole_interfaces/msg/Action]             - Desired acceleration of the cart
+    cartpole/action [cartpole_interfaces/msg/Action]             - Desired effort of the cart
 
 Sends requests:
     cartpole/stop [cartpole_interfaces/srv/Stop]                 - Stops robot
@@ -64,17 +67,17 @@ anything else: stop
 CTRL-C to quit
 """
 
-key_bindings = {'a': 1.0,
+effort_key_bindings = {'a': 1.0,
                 'd': -1.0}
 
-def get_key(settings):
-    if sys.platform == 'win32':
-        key = msvcrt.getwch()
-    else:
-        tty.setraw(sys.stdin.fileno())
-        key = sys.stdin.read(1)
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+effort_multiplier = 1.0
+
+effort_delta_key_bindings = {'z': 1.0,
+                'x': -1.0}
+
+k_queue = Queue()
+
+exit_signal = threading.Event()
 
 
 def get_terminal_settings():
@@ -83,19 +86,29 @@ def get_terminal_settings():
     return termios.tcgetattr(sys.stdin)
 
 
-class CartepoleTeleop(Node):
+class CartpoleTeleop(Node):
 
     def __init__(self):
         rclpy.init()
         super().__init__('cartpole_teleop_key')
 
-        self.topic = '/slider_cart_velocity_controller/command'
+        self.effort_topic = '/slider_cart_effort_controller/command'
+        self.velocity_topic = '/slider_cart_velocity_controller/command'
+        self.position_topic = '/slider_cart_position_controller/command'
 
         self.stop_service_name = '/cartpole/stop'
         self.set_position_service_name = '/cartpole/set_position'
 
-        self.publisher_ = self.create_publisher(Float64,
-                                                self.topic,
+        self._effort_publisher = self.create_publisher(Float64,
+                                                self.effort_topic,
+                                                10)
+
+        self._velocity_publisher = self.create_publisher(Float64,
+                                                self.velocity_topic,
+                                                10)
+
+        self._position_publisher = self.create_publisher(Float64,
+                                                self.position_topic,
                                                 10)
 
         # Clients to send the "Stop" and "Set Position" request
@@ -115,24 +128,36 @@ class CartepoleTeleop(Node):
         # Initializes Action interface for publisher
         self.msg = Float64()
 
-        # Limits the value of acceleration
-        self.max_accel = 1.0
-        self.min_accel = -self.max_accel
+        self.effort_multiplier = 15.0
 
-        self.delta_accel = 0.1
-        self.accel = 0.0
+        # Limits the value of effort
+        self.max_effort = 40.0
+        self.delta_effort = 1
+        self.effort = 0.0
 
-    def publish_accel(self):
-        self.msg.data = self.accel
+        self.velocity = 0.0
 
-        self.publisher_.publish(self.msg)
-        self.get_logger().info('Publishing: "%s" to topic: "%s"' % (self.msg.data, self.topic))
+    def publish_effort(self):
+        self.msg.data = self.effort
 
-    def limit_accel(self):
-        if self.accel > self.max_accel:
-            self.accel = self.max_accel
-        elif self.accel < self.min_accel:
-            self.accel = self.min_accel
+        self._effort_publisher.publish(self.msg)
+        self.get_logger().info('Publishing: "%s" to topic: "%s"\n' % (self.msg.data, self.effort_topic))
+
+    def publish_velocity(self):
+        self.msg.data = self.velocity
+        self._velocity_publisher.publish(self.msg)
+        self.get_logger().info('Publishing: "%s" to topic: "%s"\n' % (self.msg.data, self.velocity_topic))
+
+    def publish_position(self):
+        self.msg.data = self.position
+        self._position_publisher.publish(self.msg)
+        self.get_logger().info('Publishing: "%s" to topic: "%s"\n' % (self.msg.data, self.position_topic))
+
+    def limit_effort(self):
+        if self.effort > self.max_effort:
+            self.effort = self.max_effort
+        elif self.effort < self.min_effort:
+            self.effort = self.min_effort
 
     def send_stop_request(self):
         self.stop_request_future = self.stop_robot_client_.call_async(self.stop_request)
@@ -151,24 +176,56 @@ def check_for_service_response(node, future, msg):
             else:
                 node.get_logger().info(msg)
 
+def get_key(settings, key):
+    while not exit_signal.is_set():
+        if sys.platform == 'win32':
+            key = msvcrt.getwch()
+        else:
+            tty.setraw(sys.stdin.fileno())
+            key = sys.stdin.read(1)
+            k_queue.put(key)
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        time.sleep(0.1)
+
+def pub_cmd(node):
+    while not exit_signal.is_set():
+        if not k_queue.empty():
+            key = k_queue.get()
+            if key:
+                node.get_logger().info(key)
+                if key in effort_key_bindings:
+                    node.effort = node.effort_multiplier * effort_key_bindings[key]
+                elif key in effort_delta_key_bindings:
+                    node.publish_velocity()
+                    node.effort = 0.0
+                    node.effort_multiplier += node.delta_effort * effort_delta_key_bindings[key]
+                    continue
+                elif key == '\x03':
+                    break
+        
+        node.publish_effort()
+        time.sleep(1/30.0)
+
 
 def main():
     settings = get_terminal_settings()
-    node = CartepoleTeleop()
+    node = CartpoleTeleop()
 
     print(msg)
+    t1 = threading.Thread(target=get_key, args=(settings,k_queue))
+    t2 = threading.Thread(target=pub_cmd, args=(node,))
 
-    while True: # Input loop
-        key = get_key(settings)
-        print(key)
+    t1.start()
+    t2.start()
 
-        if key in key_bindings:
-            node.accel = key_bindings[key]
-        elif key == '\x03':
-            break
+    try:
+        while not exit_signal.is_set():  # enable children threads to exit the main thread, too
+            time.sleep(0.1)  # let it breathe a little
+    except KeyboardInterrupt:  # on keyboard interrupt...
+        exit_signal.set() 
 
-        node.limit_accel()
-        node.publish_accel()
+    t1.join()
+    t2.join()
     
     # Sends request to service
     node.send_stop_request()
